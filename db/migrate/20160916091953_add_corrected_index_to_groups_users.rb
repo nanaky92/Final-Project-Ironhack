a5358236df4f421755c13bfd76ea47554ed3a2bd
nanaky92 <i.crespo1992@gmail.com>
2016-09-19 11:18:08 +0200

    Heroku again

--- db/migrate/20160916091953_add_corrected_index_to_groups_users.rb
+++
@@ -1,7 +0,0 @@
-class AddCorrectedIndexToGroupsUsers < ActiveRecord::Migration[5.0]
-  def change
-    remove_index :groups_users, [nil, nil]
-    add_index :groups_users, [:group_id, :user_id], :unique => true
-
-  end
-end
