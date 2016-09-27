e17de45e175332dfb2cf8eef99aff920af2e072e
nanaky92 <i.crespo1992@gmail.com>
2016-09-27 23:06:26 +0200

    Heroku

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,5 +1,22 @@
 class Api::EventsController < ApplicationController
   def send_reminders
-    # GroupMailer.
+    @user = User.find(session[:id])
+    @group = Group.find(params["group"])
+    @event = Event.find(params["event"])
+
+    if @group.isUserAdmin?(@user)
+      @appointments = @event.appointments
+      @group.users.each do |user|
+        EventMailer.reminder_votation(user, @group, @event).deliver_later
+      end
+      render json: {
+        message: "Reminders sent",
+        }, status: 200 
+    else
+      render json: {
+        message: "You don't have permission for this",
+        }, status: 400 
+    end
   end
+
 end
