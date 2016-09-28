b1747992725c4ccfecdf77b925c48701e1dd7426
nanaky92 <i.crespo1992@gmail.com>
2016-09-28 02:23:39 +0200

    Heroku

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,29 +1,29 @@
 class Api::EventsController < ApplicationController
-  # def send_reminders
-  #   @user = User.find(session[:id])
-  #   @group = Group.find(params["group"])
-  #   @event = Event.find(params["event"])
-
-  #   if @group.isUserAdmin?(@user)
-
-  #     filter_users.each do |user|
-  #       EventMailer.reminder_votation(user, @group, @event).deliver_later
-  #     end
-  #     render json: {
-  #       message: "Reminders sent",
-  #       }, status: 200 
-  #   else
-  #     render json: {
-  #       message: "You don't have permission for this",
-  #       }, status: 400 
-  #   end
-  # end
-
   def send_reminders
+    @user = User.find(session[:id])
+    @group = Group.find(params["group"])
+    @event = Event.find(params["event"])
+
+    if @group.isUserAdmin?(@user)
+
+      filter_users.each do |user|
+        EventMailer.reminder_votation(user, @group, @event).deliver_later
+      end
       render json: {
         message: "Reminders sent",
         }, status: 200 
+    else
+      render json: {
+        message: "You don't have permission for this",
+        }, status: 400 
+    end
   end
+
+  # def send_reminders
+  #     render json: {
+  #       message: "Reminders sent",
+  #       }, status: 200 
+  # end
 
   def filter_users
     @votations = @event.votations.find_by(access: true)
