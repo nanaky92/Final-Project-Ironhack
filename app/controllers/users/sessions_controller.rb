dacc9392eabcdb793fbff3de83d3aaa663ade306
nanaky92 <i.crespo1992@gmail.com>
2016-09-28 02:33:38 +0200

    Heroku

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,13 @@
 class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  GET /resource/sign_in
+  def new
+    super
+    session[:id] = @user.id
+    cookies[:user_name] = "david"
+
+  end
 
   # POST /resource/sign_in
   # def create
